8ff181a3cd69067214fd2741e66e96aeb8eb8edc
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2020-09-25 16:39:49 +0200

    Bugfix guards

--- src/app/auth/admin-auth.guard.ts
+++ src/app/auth/admin-auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from '../services/auth.service';
 
@@ -7,13 +7,16 @@ import {AuthService} from '../services/auth.service';
   providedIn: 'root'
 })
 export class AdminAuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
   // solo chi ha ruolo admin può attivare certi percorsi
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.getRole() === 'ROLE_ADMIN';
+    if (!(this.authService.getRole() === 'ROLE_ADMIN')){
+      this.router.navigate(['not-found']);
+      return false;
+    }
+    return true;
   }
-
 }
--- src/app/auth/teacher-auth.guard.ts
+++ src/app/auth/teacher-auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from '../services/auth.service';
 
@@ -7,14 +7,16 @@ import {AuthService} from '../services/auth.service';
   providedIn: 'root'
 })
 export class TeacherAuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
   // solo chi ha ruolo professor può attivare certi percorsi
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.getRole() === 'ROLE_PROFESSOR';
-
+    if (!(this.authService.getRole() === 'ROLE_PROFESSOR')){
+      this.router.navigate(['not-found']);
+      return false;
+    }
+    return true;
   }
-
 }
